26cf31f8ea91a91c534e65bd462a8407a171e712
Achsmboh <maulana.achsanxiipa1@gmail.com>
2022-10-15 10:31:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, optio voluptatibus facere cupiditate modi dolorem magnam aliquam voluptas pariatur iure delectus nihil fugit quam numquam quo harum aperiam vel ab?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
